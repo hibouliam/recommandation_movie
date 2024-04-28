ef50d96ef2dde5e1de1087d0cca66f1098f75565
hibouliam <166120998+hibouliam@users.noreply.github.com>
2024-04-28 20:30:04 +0200

    new files update

--- link.py
+++ link.py
@@ -4,32 +4,125 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from user_user import predict_without_cos,modify_row_matrix,add_row_matrix,sort_users_near_userid,take_rating
 from sklearn.feature_extraction.text import CountVectorizer
+from element_element import found_movie_from_name, add_rows,find_near_movies,sort_movie
 from sklearn.metrics.pairwise import cosine_similarity
 
-df=pd.read_csv('links.csv')
-df=df.head(20)
-print(df.head())
+links=pd.read_csv('links.csv')
+data=pd.read_csv('movies_metadata.csv')
+matrix=pd.read_csv('matrix_test.csv')
+dt = pd.read_csv('credits.csv')
+#fusionne les deux colonnes en fonction de l'Id
+data['id'] = pd.to_numeric(data['id'], errors='coerce')
+data = pd.merge(data, dt, left_on='id', right_on='id', how='inner')
+warnings.filterwarnings('ignore')
+#Initialisation demande
 
-id='tt0210234'
-print(int(id[2:]))
+list_movie_note_user=[]
+
 
 #Première étape on demande les films qu'il aime en y mettant une note
+while len(list_movie_note_user)<=1:
+    movie=input("Veuillez entrer un film : ")
+    row_data=found_movie_from_name(data,movie)
+    if not row_data.empty:
+        note=input("Veuillez entrer une note : ")
+        list_movie_note_user.append([movie,row_data,note,row_data['imdb_id'].iloc[0][2:]])
+        #print([movie,row_data["imdb_id"],note])
+        #print(row_data['imdb_id'].iloc[0][2:])
+#print(list_movie_note_user)
 
-#Deuxième étape on utilise les similarités élément_élément qui
  
-#Retourne dix id imdb 
+
+#Deuxième étape on utilise les similarités élément_élément qui retourne dix id imdb 
+
+total_rows=len(data)
+batch_size=1000
+similar_movies=[]
+similar_movies_imdb=[]
+
+for i in range(0,total_rows,batch_size):
+    result=[]
+    d1 = data.iloc[i:i+batch_size]
+    for movie_note_user in list_movie_note_user :
+        #print(movie_note_user[1]) #vérification
+        d1=add_rows(d1,movie_note_user[1])
+    
+    similar_movies1=find_near_movies(d1,sort_movie(d1),len(list_movie_note_user))
+    
+    for t in similar_movies1:  
+        #print(t[0],t[1], type(t[0]))
+        modified_tuple = (int(t[0])+i-1, t[1])
+
+        result.append(modified_tuple)
+    
+    similar_movies+=result
+
+similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)[0:25]
+#print("aba",similar_movies)
+unique_movies = set()
+final_similar_movies = []
+
+for movie in similar_movies:
+    if movie[0] not in unique_movies:
+        final_similar_movies.append(movie)
+        unique_movies.add(movie[0])
+
+similar_movies = sorted(final_similar_movies, key=lambda x: x[1], reverse=True)
+#print(similar_movies)
+for movie_index, similarity in similar_movies:
+    similar_movies_imdb.append([data.iloc[movie_index]['title'],int(data.iloc[movie_index]['imdb_id'][2:])]) 
+#print("imdb",similar_movies_imdb)
 
 #On trouve l'id dans pour le fichier rating 
-def link_between_moviesid(movie_imdb):
-    for item in range(len(df)):
-        if int(df.iloc[item][1]) == movie_imdb :
-            return df.iloc[item][0] 
-    return False 
+def link_between_moviesid(links,movie_imdb):
+    for item in range(len(links)):
+        value = links.iloc[item][1]
+        if not pd.isnull(value) and int(value) == movie_imdb:
+            #print (links.iloc[item][0])
+            return links.iloc[item][0] 
+    return None
 
 
 #On crée une nouvelle ligne à la matrice ratings en ajoutant les nouveaux id movie
+id_user=len(matrix)
+matrix=add_row_matrix(matrix,id_user)
+for movie in list_movie_note_user :
+    #print(movie)
+    note_user=movie[2]
+    #print(note_user)
+    id_imdb=movie[3]
+    #print(id_imdb)
+    id_imdb=int(id_imdb)
+    
+    moviesid=link_between_moviesid(links, id_imdb)
+    matrix=modify_row_matrix(matrix,id_user,moviesid,note_user) 
 
 #Et on utilise l'algo user user pour trouver la note qu'il mettrait au film similaire
 
+near_user=8
+id_user=len(matrix)-1 #utilisateur 7 pour film 1 ligne 10 qd 8
+predict_rating_list =[]
+#print(matrix)
+#print(similar_movies_imdb)
+similar_movies_imdb=similar_movies_imdb[0:3]
+sort,nbr_movie_rate=sort_users_near_userid(matrix, id_user)
+for movie_imdb in similar_movies_imdb :
+    predicted_rating =False
+    #print(movie_imdb)
+    id_imdb=movie_imdb[1]
+    #print(id_imdb)
+    id_movie=str(int(link_between_moviesid(links, id_imdb))) 
+    #print('TRans',id_movie)
+    #print(id_user)
+    #print(near_user)
+    
+    predicted_rating,user= take_rating(matrix,sort,id_movie,near_user,nbr_movie_rate)
+    #print(predicted_rating)
+    predict_rating_list.append([movie_imdb,predicted_rating,user])
+    print(predict_rating_list)  
 #Et on affiche les films qui ont une note supérieur à une note prédéfénite
+for movie in predict_rating_list :
+    if (movie[1]>=4.0 and movie[2]<10) :
+        print(movie[0]) #c'est le numéro imdb et titre du film qui est recommandé 
--- user_user.py
+++ user_user.py
@@ -4,7 +4,7 @@ Created on Sun Mar 24 13:23:02 2024
 
 @author: huiss
 """
-
+import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from math import sqrt
@@ -12,6 +12,7 @@ from math import sqrt
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
 
 '''# Charger les données
 data = pd.read_csv('ratings.csv')
@@ -31,32 +32,119 @@ def create_rating_matrix (data,start,total_rows, batch_size):
     z=0
     ratings_matrices = []
     for i in range(start, total_rows, batch_size):
-        # Sélectionner un lot de données
+    # Sélectionner un lot de données
+
         batch_data = data.iloc[i:i+batch_size]
+        print(batch_data)
+        print(type(batch_data))
         # Créer la matrice de notation pour ce lot
         ratings_matrix_batch = batch_data.pivot_table(index='userId', columns='movieId', values='rating').fillna(0)
-        
+        print(ratings_matrix_batch)
         # Ajouter la matrice de notation à la liste de résultats
         ratings_matrices.append(ratings_matrix_batch)
+        print(ratings_matrices)
+        print(type(ratings_matrices))
         z+=1
         print(z)
-   
+
     # Concaténer les matrices de notation de tous les lots en une seule
     ratings_matrix = pd.concat(ratings_matrices)
+    print(ratings_matrix)
     ratings_matrix = ratings_matrix.fillna(0)
     print(ratings_matrix)
-    ratings_matrix.to_csv(r'matrix.csv', index=False)
+    ratings_matrix.to_csv(r'matrix_test.csv', index=False)
     return ratings_matrix
+
+def add_row_matrix(matrix,user) :
+    new_row = pd.DataFrame({'1': [0.0]}, index=[user])
+    df = pd.concat([matrix,new_row])
+    df = df.fillna(0)
+    return df
+
+def modify_row_matrix(matrix, user, id_film, note) :
+    matrix.loc[int(user), str(int(id_film))] = note 
+    return matrix
+
+def sort_users_near_userid(ratings_matrix,user_id) :
+   
+    user_ratings = ratings_matrix.loc[user_id]
+    user_ratings_nonzero = user_ratings[user_ratings != 0]
+    print("user_ratings_nonzero",user_ratings_nonzero)
+    nbre_movie_rate=len(user_ratings_nonzero)
+    ratings_matrix_nonzero = ratings_matrix[user_ratings_nonzero.index]
+    print("ratings_matrix_nonzero",[ratings_matrix_nonzero])
+    print("ratings_matrix_nonzero",user_ratings_nonzero)
+    print(len(ratings_matrix_nonzero))
+    list_user_score=[]
+    for user in range(0,len(ratings_matrix_nonzero )):
+        score=0
+        index=0
+        number_movie_similar=0
+        if not user==user_id :
+            #print("i",ratings_matrix_nonzero.iloc[user])
+            for rating in ratings_matrix_nonzero.iloc[user] :
+                #print(rating)
+                if rating != 0.0 :
+                    #print( user_ratings_nonzero.iloc[index],type( user_ratings_nonzero.iloc[index]))
+                    score += np.abs(rating - float(user_ratings_nonzero.iloc[index]))
+                    number_movie_similar+=1
+                
+                index+=1
+            if not number_movie_similar==0 :
+                list_user_score.append([user,score,number_movie_similar])
+            list_user_score_sort = sorted(list_user_score, key=lambda x: x[2],reverse=True)
+            list_user_score_sort = sorted(list_user_score_sort, key=lambda x: x[1])
+            
+    return list_user_score_sort,nbre_movie_rate
+
+def take_rating(ratings_matrix,list_user_score,movie_id,k,nbr_movie_rate):
+    predicted_rating =[]
+    number_iteration=0
+    print(number_iteration,len(predicted_rating))
+    while number_iteration<k or len(predicted_rating)==0 :
+    
+        print('list',list_user_score[number_iteration][0])
+        user_id=int(list_user_score[number_iteration][0])
+        similar_users_ratings = ratings_matrix.iloc[user_id][movie_id]
+        #print("similar_users_ratings",similar_users_ratings)
+        #print("similar_users_ratings",similar_users_ratings)
+        #print((similar_users_ratings))
+        
+        if similar_users_ratings != 0 and (list_user_score[number_iteration][2]>=(nbr_movie_rate)//2 or list_user_score[number_iteration][2]>=5):
+            predicted_rating.append(similar_users_ratings)
+        number_iteration+=1
+    print(predicted_rating)
+    print((nbr_movie_rate)//2)
+    predicted_rating = sum(predicted_rating)/len(predicted_rating)    
+    print(predicted_rating,number_iteration)
+    return predicted_rating,number_iteration
+
+def predict_without_cos(data,user_id,id_movie,near_user) :
+
+    sort,nbr_movie_rate=sort_users_near_userid(data, user_id)
+    print(sort,nbr_movie_rate)
+    predicted_rating,near_user_end = take_rating(data,sort,id_movie,near_user,nbr_movie_rate)
+    return predicted_rating,near_user_end
 
 # Fonction pour prédire les notes
 def predict(ratings_matrix, user_id, item_id, k):
+    
+    print("itemi",item_id,type(item_id))
     user_ratings = ratings_matrix.loc[user_id]
-    #print("user_ratings",user_ratings)
     user_ratings_nonzero = user_ratings[user_ratings != 0]
-    #print("user_ratings_nonzero",user_ratings_nonzero)
+    print("user_ratings_nonzero",user_ratings_nonzero)
     ratings_matrix_nonzero = ratings_matrix[user_ratings_nonzero.index]
-    #print("ratings_matrix_nonzero",ratings_matrix_nonzero)
-    user_similarities = cosine_similarity([user_ratings_nonzero], ratings_matrix_nonzero)[0]
+    print("ratings_matrix_nonzero",[ratings_matrix_nonzero])
+    print("ratings_matrix_nonzero",user_ratings_nonzero)
+    donnees = {'1': [100.0]}
+    df = pd.DataFrame(donnees)
+    df_normalized = normalize(df)
+    print(df_normalized)
+    print(type(ratings_matrix_nonzero),type([user_ratings_nonzero]),type(df))
+    print(ratings_matrix_nonzero)
+    
+    user_similarities = cosine_similarity([user_ratings_nonzero], normalize(ratings_matrix_nonzero))[0]
+    print(user_similarities)
     similar_users_indices = np.argsort(user_similarities)[::-1][1:k+1]
     #print("similar_users_indices",similar_users_indices)
     #print("ratings_matrix.iloc[similar_users_indices]['1']",ratings_matrix.iloc[similar_users_indices][item_id])
@@ -64,13 +152,15 @@ def predict(ratings_matrix, user_id, item_id, k):
     similar_users_ratings = ratings_matrix.iloc[similar_users_indices][item_id]
     similar_users_ratings = similar_users_ratings[similar_users_ratings != 0]
     #print("similar_users_ratings",similar_users_ratings)
+    print((similar_users_ratings))
     if len(similar_users_ratings) == 0:
-        k+=1
-        predict (ratings_matrix,user_id,item_id,k) # Retourner 0 si aucun utilisateur similaire n'a noté ce film
-    
-    predicted_rating = similar_users_ratings.mean()
+        return False,k
+    else :
+        predicted_rating = similar_users_ratings.mean()
     
     return predicted_rating,k
+
+
 
 '''
 predicted_rating=0
@@ -93,46 +183,51 @@ for i in item_id :
     
     print(f"La note prédite pour l'utilisateur {user_id} et le film {i} est: {predicted_rating}")
 '''
-#data = pd.read_csv('ratings.csv')
-#batch_size = 100000
-#create_rating_matrix(data,0, 500000, batch_size)
-#df1=pd.read_csv("concatenated_file.csv")
+#Création des matrices a enregitrer en csv
+'''
+#Création des matrices a enregitrer en csv
+data = pd.read_csv('ratings_small.csv')
+data=data.head(99)
+print(data)
+batch_size = len(data)*2
+print(batch_size)
+
+create_rating_matrix(data,0, len(data), batch_size)
+'''
+
+#Concaténation des matrices
+#df1=pd.read_csv("matrix.csv")
 #print(df1)
 '''df2=pd.read_csv("matrix1.csv")
 print(df2)
 concatenated_df = pd.concat([df1, df2])
 concatenated_df.to_csv('concatenated_file.csv', index=False)
 print("wesh")'''
-
-
-data = pd.read_csv('matrix.csv')
-print(data)
-predicted_rating,k= predict(data, 2, '1', k=3)
-print(predicted_rating,k)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
+data = pd.read_csv('matrix_test.csv')
+data=add_row_matrix(data,672)
+data=modify_row_matrix(data,672,'2',1000000.0)
+predicted,y=predict_without_cos(data,672,'23',3)
+print(predicted,y)
+print(y)
+#Prediction des films en fonction du numéro de l'utilisateur et numéro de film
+'''
+'''
+data = pd.read_csv('matrix_test.csv')
+data=add_row_matrix(data,672)
+data=modify_row_matrix(data,672,'2',1000000.0)
+predict=predict_without_cos(data,673,'23',near_user)
+print(predict)
+
+
+
+
+
+
+
+
+
+
 # Tester la prédiction pour un utilisateur et un film spécifiques
 user_id = 7
 item_id = 1408
@@ -175,4 +270,71 @@ item_id = 1405
 predicted_rating= predict(ratings_matrix, user_id, item_id, k=1000)
 
 print(f"La note prédite pour l'utilisateur {user_id} et le film {item_id} est: {predicted_rating}",z)
+'''
+'''
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
+import numpy as np
+
+def compute_difference(row1, row2):
+    # Calculer la différence absolue entre chaque paire de valeurs correspondantes
+    differences = np.abs(row1 - row2)
+    # Somme des différences absolues
+    total_difference = np.sum(differences)
+    return total_difference
+
+# Définir une matrice de vecteurs
+X = np.array([[1, 2, 3], [4, 5, 6], [1, 5, 6]])  # Matrice X
+
+# Indice de la ligne de référence
+ligne_reference = 0
+
+# Extraire la ligne de référence
+reference = X[ligne_reference]
+
+# Initialiser les variables pour stocker la meilleure correspondance et la différence minimale
+meilleure_correspondance = None
+difference_minimale = float('inf')  # Initialiser à l'infini pour garantir qu'elle sera remplacée lors de la première itération
+
+# Parcourir toutes les autres lignes
+for i, ligne in enumerate(X):
+    print(ligne,i)
+    if i != ligne_reference:  # Ignorer la ligne de référence elle-même
+        # Calculer la différence entre la ligne de référence et la ligne actuelle
+        difference = compute_difference(reference, ligne)
+        # Mettre à jour la meilleure correspondance si la différence est minimale
+        if difference < difference_minimale:
+            difference_minimale = difference
+            meilleure_correspondance = i
+
+# Afficher l'indice de la ligne la plus similaire
+print("L'indice de la ligne la plus similaire à la première ligne est :", meilleure_correspondance)
+'''
+
+''' 
+        
+
+
+data=add_row_matrix(data,673)
+data=modify_row_matrix(data,673,'2',5.0)
+data=modify_row_matrix(data,673,'3',5.0)
+data=modify_row_matrix(data,673,'7',3.0)
+data=modify_row_matrix(data,673,'1',2.0)
+data=modify_row_matrix(data,673,'9',5.0)
+data=modify_row_matrix(data,673,'8',0.5)
+data=modify_row_matrix(data,673,'10',4.5)
+print(data)
+print(len(data))
+near_user=3
+predicted_rating=False
+print(sort_users_near_userid(data, 673))
+def predict_without_cos(data,user_id,id_movie,near_user) :
+
+    sort,nbr_movie_rate=sort_users_near_userid(data, user_id)
+    print(sort,nbr_movie_rate)
+    predicted_rating = take_rating(data,sort,id_movie,near_user,nbr_movie_rate)
+    return predicted_rating,near_user
+print(predict_without_cos(data,673,'23',near_user))
+
 '''
