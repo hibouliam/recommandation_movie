08e6c30ed6569b0ea7cfb0035f112f8e442ea9ac
huis <huissoudliam@aol.com>
2024-05-18 18:48:40 +0200

    nouvelle adresse des fichiers

--- api/data_reader.py
+++ api/data_reader.py
@@ -7,12 +7,12 @@ def get_links():
     return pd.read_csv(gcs_url)
 
 def get_data():
-    gcs_url = "gs://movie_recommendation_1/movies_metadata_updated.csv"
+    gcs_url = "gs://movie_recommendation_1/filtered_movies_data.csv"
     return pd.read_csv(gcs_url)
 
 
 def get_matrix():
-    gcs_url = "gs://movie_recommendation_1/matrix_test.csv"
+    gcs_url = "gs://movie_recommendation_1/matrix_1000000_users.csv"
     return pd.read_csv(gcs_url)
 
 
